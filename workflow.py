3bee253f279c7273fe5c08d2ee86b0b9c1d6358c
Ben Clifford <benc@hawaga.org.uk>
2019-06-03 15:21:15 -0700

    split in-bash multiple commands into some more topical separate apps

--- workflow.py
+++ workflow.py
@@ -17,6 +17,8 @@ parsl.set_stream_logger(__name__)
 
 logger.info("Logging should be initialised now")
 
+global_repo="DATA_GR"
+
 logger.info("Getting checkpoint files")
 config.checkpoint_files = parsl.utils.get_all_checkpoints()
 logger.info("Checkpoint files: {}".format(config.checkpoint_files))
@@ -29,11 +31,19 @@ logger.info("Defining tutorial import subroutine")
 # be hardlinked or copied? so that the ci_hsc stuff can be
 # a transient working directory?
 
+@parsl.bash_app(cache=True)
+def create_empty_repo(repo: str):
+    return "rm -rf {r} && mkdir {r} && echo lsst.obs.hsc.HscMapper > {r}/_mapper".format(r=repo)
+
+@parsl.bash_app(cache=True)
+def install_transmission_curves(repo: str):
+    return "installTransmissionCurves.py {r}".format(r=repo)
+
 # the tutorial says should use ingestCalibs but they take a short cut
 # and do it the "wrong" way using ln - could replace that?
 @parsl.bash_app(cache=True)
-def import_ci_hsc(stdout="ingest.default.stdout", stderr="ingest.default.stderr"):
-    return "rm -rf DATA && rm -rf ci_hsc && git clone https://github.com/lsst/ci_hsc && setup -j -r ci_hsc && mkdir DATA && echo lsst.obs.hsc.HscMapper > DATA/_mapper && ingestImages.py DATA $CI_HSC_DIR/raw/*.fits --mode=link && installTransmissionCurves.py DATA && ln -s $CI_HSC_DIR/CALIB/ DATA/CALIB && mkdir -p DATA/ref_cats && ln -s $CI_HSC_DIR/ps1_pv3_3pi_20170110 DATA/ref_cats/ps1_pv3_3pi_20170110 "
+def import_ci_hsc(repo: str, stdout="ingest.default.stdout", stderr="ingest.default.stderr"):
+    return "rm -rf ci_hsc && git clone https://github.com/lsst/ci_hsc && setup -j -r ci_hsc && ingestImages.py {r} $CI_HSC_DIR/raw/*.fits --mode=link && ln -s $CI_HSC_DIR/CALIB/ {r}/CALIB && mkdir -p {r}/ref_cats && ln -s $CI_HSC_DIR/ps1_pv3_3pi_20170110 {r}/ref_cats/ps1_pv3_3pi_20170110 ".format(r=repo)
 
 # this assumes that we're running in the same
 # python process - using thread local executor -
@@ -41,9 +51,14 @@ def import_ci_hsc(stdout="ingest.default.stdout", stderr="ingest.default.stderr"
 def tutorial_1_import():
     logger.info("starting data import")
 
-    import_future = import_ci_hsc()
+    empty_repo_future = create_empty_repo(global_repo)
+    empty_repo_future.result()
+
+    transmission_curve_future = install_transmission_curves(global_repo)
+    import_future = import_ci_hsc(global_repo)
+
+    transmission_curve_future.result()
     import_future.result()
- 
 
     logger.info("ended data import")
 
@@ -51,24 +66,31 @@ def tutorial_1_import():
 # pccd_show and pccd_process could be refactored,
 # with a bool parameter?
 @parsl.bash_app(cache=True)
-def pccd_show(stdout="pccd_show.default.stdout"):
-    return "processCcd.py DATA --rerun rr-processccd-show --id --show data"
+def pccd_show(repo: str, stdout="pccd_show.default.stdout"):
+    return "processCcd.py {r} --rerun rr-processccd-show --id --show data".format(r=repo)
 
 @parsl.bash_app(cache=True)
-def pccd_process(stdout="pccd_process.default.stdout"):
-    return "processCcd.py DATA --rerun rr-processccd-show --id"
+def pccd_process(repo: str, stdout="pccd_process.default.stdout"):
+    return "processCcd.py {r} --rerun rr-processccd-show --id".format(r=repo)
 
 def tutorial_2_show_data():
-    logger.info("asking processCcd.py to show data")
+    logger.info("running some processCcd task")
     # These two could run in parallel as a demo of running stuff
     # in parallel
-    pccd_show_future = pccd_show()
-    pccd_show_future.result()
 
-    pccd_process_future = pccd_process()
+    # ideally they'd not take global_repo as an input, but some descriptor
+    # that would come back from the tutorial_1_import stage...
+    # and then the earlier tasks would not have to do a result wait at all
+    # but instead the descriptor would live inside a future and provide
+    # the dependencies there.
+
+    pccd_show_future = pccd_show(global_repo)
+    pccd_process_future = pccd_process(global_repo)
+
+    pccd_show_future.result()
     pccd_process_future.result()
 
-    logger.info("finished asking processCcd.py to show data")
+    logger.info("finished processCcd tasks")
 
 
 parsl.load(config)
